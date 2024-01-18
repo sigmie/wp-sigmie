72669fc7e28f17544a58ca7f68cc1738b8dc0ca1
Nico Orfanos <nico@sigmie.com>
2024-01-18 14:32:56 +0200

    wip

--- admin/class-sigmie-admin-page-search.php
+++ admin/class-sigmie-admin-page-search.php
@@ -95,15 +95,100 @@ class Sigmie_Admin_Page_Search
 		);
 	}
 
+	public function search_show_loader_callback()
+	{
+		$value = (string) get_option('sigmie_show_loader', '1');
+?>
+		<fieldset class="">
+			<span class="">
+				<input type="radio" id="sigmie_show_loader_on" name="sigmie_show_loader" value="1" <?php checked($value, '1'); ?> />
+				<label class="checkbox" for="sigmie_show_loader_on"><?php _e('Yes'); ?></label>
+			</span>
+			<br>
+			<span class="">
+				<input type="radio" id="sigmie_show_loader_off" name="sigmie_show_loader" value="0" <?php checked($value, '0'); ?> />
+				<label class="checkbox" for="sigmie_show_loader_off"><?php _e('No'); ?></label>
+			</span>
+		</fieldset>
+	<?php
+	}
+
+	public function search_field_height_callback()
+	{
+		$value = (string) get_option('sigmie_search_field_height', 50);
+	?>
+		<input type="number" name="sigmie_search_field_height" class="regular-text" value="<?php echo esc_html($value); ?>"></input>
+		<p class="description" id="home-description">
+			<?php esc_html_e('Search field maximum height in pixels.', 'sigmie'); ?>
+		</p>
+	<?php
+	}
+
+	public function search_field_width_callback()
+	{
+		$value = (string) get_option('sigmie_search_field_width', 999);
+	?>
+		<input type="number" name="sigmie_search_field_width" class="regular-text" value="<?php echo esc_html($value); ?>"></input>
+		<p class="description" id="home-description">
+			<?php esc_html_e('Search field maximum width in pixels.', 'sigmie'); ?>
+		</p>
+	<?php
+	}
+
 	public function search_field_text_callback()
 	{
-		$value = (string) get_option('search_field_text', '');
-?>
-		<input type="text" name="search_field_text" class="regular-text" value="<?php echo esc_html($value); ?>"></input>
+		$value = (string) get_option('sigmie_search_field_text', '');
+	?>
+		<input type="text" name="sigmie_search_field_text" class="regular-text" value="<?php echo esc_html($value); ?>"></input>
 		<p class="description" id="home-description">
 			<?php esc_html_e('Text for search field placeholder.', 'sigmie'); ?>
 		</p>
 <?php
+	}
+
+	public function sanitize_search_field_width($value)
+	{
+		$value = sanitize_text_field($value);
+
+		if (empty($value)) {
+			add_settings_error(
+				$this->option_group,
+				'empty',
+				esc_html__('Search field width value shoud no be empty', 'sigmie')
+			);
+		}
+
+		return $value;
+	}
+
+	public function sanitize_search_field_height($value)
+	{
+		$value = sanitize_text_field($value);
+
+		if (empty($value)) {
+			add_settings_error(
+				$this->option_group,
+				'empty',
+				esc_html__('Search field height value shoud no be empty', 'sigmie')
+			);
+		}
+
+		return $value;
+	}
+
+	public function sanitize_show_loader($value)
+	{
+		$value = sanitize_text_field($value);
+
+		if (!in_array($value, ['0', '1'])) {
+			add_settings_error(
+				$this->option_group,
+				'empty',
+				esc_html__('Show loader value is invalid', 'sigmie')
+			);
+		}
+
+		return $value;
 	}
 
 	public function sanitize_search_field_text($value)
@@ -148,8 +233,50 @@ class Sigmie_Admin_Page_Search
 
 		register_setting(
 			$this->option_group,
-			'search_field_text',
+			'sigmie_search_field_text',
 			array($this, 'sanitize_search_field_text')
+		);
+
+		add_settings_field(
+			'show_loader',
+			esc_html__('Show loader', 'sigmie'),
+			array($this, 'search_show_loader_callback'),
+			$this->slug,
+			$this->section
+		);
+
+		register_setting(
+			$this->option_group,
+			'sigmie_show_loader',
+			array($this, 'sanitize_show_loader')
+		);
+
+		add_settings_field(
+			'search_field_height',
+			esc_html__('Search field height', 'sigmie'),
+			array($this, 'search_field_height_callback'),
+			$this->slug,
+			$this->section
+		);
+
+		register_setting(
+			$this->option_group,
+			'sigmie_search_field_height',
+			array($this, 'sanitize_search_field_height')
+		);
+
+		add_settings_field(
+			'search_field_width',
+			esc_html__('Search field width', 'sigmie'),
+			array($this, 'search_field_width_callback'),
+			$this->slug,
+			$this->section
+		);
+
+		register_setting(
+			$this->option_group,
+			'sigmie_search_field_width',
+			array($this, 'sanitize_search_field_width')
 		);
 	}
 
@@ -185,5 +312,6 @@ class Sigmie_Admin_Page_Search
 	 */
 	public function print_section_settings()
 	{
+		echo '<div class="notice"><p>You can use it as widgets, you will find inside the widgets areas or you can use the shortcode [sigmie_search_bar].</p> </div>';
 	}
 }
