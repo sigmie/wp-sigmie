5877ef9985e5b1ee3ad625c8ee4f506828ee837a
Nico Orfanos <nico@sigmie.com>
2024-02-14 17:46:33 +0100

    wip

--- resources/PriceSlider.vue
+++ resources/PriceSlider.vue
@@ -1,55 +1,17 @@
 <template>
   <div class="sgm-relative sgm-w-full">
     <div
-      class="sgm-h-12 sgm-ct-chart sgm-ct-chart-pie sgm-ct-perfect-fourth"
+      class="sgm-h-12 ct-chart sgm-ct-chart-pie sgm-ct-perfect-fourth"
     ></div>
     <div class="sgm-px-2">
       <Slider
+        :range="true"
         :min="min"
         :max="max"
         :step="1"
-        :value="range"
+        :modelValue="range"
         @update:modelValue="onSliderChange"
-        :lazy="false"
-        :tooltips="false"
         class="sgm-mt-2"
-        :classes="{
-          target:
-            'sgm-relative sgm-box-border sgm-select-none sgm-touch-none sgm-tap-highlight-transparent sgm-touch-callout-none sgm-disabled:cursor-not-allowed',
-          focused: 'sgm-slider-focused',
-          tooltipFocus: 'sgm-slider-tooltip-focus',
-          tooltipDrag: 'sgm-slider-tooltip-drag',
-          ltr: 'sgm-slider-ltr',
-          rtl: 'sgm-slider-rtl',
-          horizontal: 'sgm-slider-horizontal sgm-h-1.5',
-          vertical: 'sgm-slider-vertical sgm-w-1.5 sgm-h-80',
-          textDirectionRtl: 'sgm-slider-txt-rtl',
-          textDirectionLtr: 'sgm-slider-txt-ltr',
-          base: 'sgm-w-full sgm-h-full sgm-relative sgm-z-1 sgm-bg-gray-300 sgm-rounded',
-          connects:
-            'sgm-w-full sgm-h-full sgm-relative sgm-overflow-hidden sgm-z-0 sgm-rounded',
-          connect:
-            'sgm-absolute sgm-z-1 sgm-top-0 sgm-right-0 sgm-transform-origin-0 sgm-transform-style-flat sgm-h-full sgm-w-full sgm-bg-gray-500 sgm-cursor-pointer sgm-tap:duration-300 sgm-tap:transition-transform sgm-disabled:bg-gray-400 sgm-disabled:cursor-not-allowed',
-          origin:
-            'sgm-slider-origin sgm-absolute sgm-z-1 sgm-top-0 sgm-right-0 sgm-transform-origin-0 sgm-transform-style-flat sgm-h-full sgm-w-full sgm-h:h-0 sgm-v:-top-full sgm-txt-rtl-h:left-0 sgm-txt-rtl-h:right-auto sgm-v:w-0 sgm-tap:duration-300 sgm-tap:transition-transform',
-          handle:
-            'sgm-absolute sgm-rounded-full sgm-bg-white sgm-border-0 sgm-shadow-slider sgm-cursor-grab sgm-focus:outline-none sgm-h:w-4 sgm-h:h-4 sgm-h:-top-1.5 sgm-h:-right-2 sgm-txt-rtl-h:-left-2 sgm-txt-rtl-h:right-auto sgm-v:w-4 sgm-v:h-4 sgm-v:-top-2 sgm-v:-right-1.25 sgm-disabled:cursor-not-allowed sgm-focus:ring sgm-focus:ring-gray-500 sgm-focus:ring-opacity-30',
-          handleLower: 'sgm-slider-hande-lower',
-          handleUpper: 'sgm-slider-hande-upper',
-          touchArea: 'sgm-h-full sgm-w-full',
-          tooltip:
-            'sgm-absolute sgm-block sgm-text-sm sgm-font-semibold sgm-whitespace-nowrap sgm-py-1 sgm-px-1.5 sgm-min-w-5 sgm-text-center sgm-text-white sgm-rounded sgm-border sgm-border-gray-500 sgm-bg-gray-500 sgm-transform sgm-h:-translate-x-1/2 sgm-h:left-1/2 sgm-v:-translate-y-1/2 sgm-v:top-1/2 sgm-disabled:bg-gray-400 sgm-disabled:border-gray-400 sgm-merge-h:translate-x-1/2 sgm-merge-h:left-auto sgm-merge-v:-translate-x-4 sgm-merge-v:top-auto sgm-tt-focus:hidden sgm-tt-focused:block sgm-tt-drag:hidden sgm-tt-dragging:block',
-          tooltipTop: 'sgm-bottom-6 sgm-h:arrow-bottom sgm-merge-h:bottom-3.5',
-          tooltipBottom: 'sgm-top-6 sgm-h:arrow-top sgm-merge-h:top-5',
-          tooltipLeft: 'sgm-right-6 sgm-v:arrow-right sgm-merge-v:right-1',
-          tooltipRight: 'sgm-left-6 sgm-v:arrow-left sgm-merge-v:left-7',
-          tooltipHidden: 'sgm-slider-tooltip-hidden',
-          active:
-            'sgm-slider-active sgm-shadow-slider-active sgm-cursor-grabbing',
-          draggable: 'sgm-cursor-ew-resize sgm-v:cursor-ns-resize',
-          tap: 'sgm-slider-state-tap',
-          drag: 'sgm-slider-state-drag',
-        }"
       />
     </div>
     <div
@@ -86,18 +48,14 @@
   </div>
 </template>
 
-<style lang="scss" scoped>
-// @import "../node_modules/@vueform/slider/themes/tailwind.scss";
-</style>
-
 <script setup>
 import { onMounted, ref, defineEmits, nextTick, watch } from "vue";
 import { BarChart } from "chartist";
 import "chartist/dist/index.css";
-import Slider from "@vueform/slider";
 import InputGroup from "primevue/inputgroup";
 import InputGroupAddon from "primevue/inputgroupaddon";
 import InputNumber from "primevue/inputnumber";
+import Slider from "primevue/slider";
 
 const props = defineProps({
   histogram: {
