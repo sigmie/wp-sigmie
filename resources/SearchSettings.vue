2f0ea5c540a4b6f2ee8e969f2977d05f545b2a2f
Nico Orfanos <nico@sigmie.com>
2024-04-06 14:44:53 +0300

    wip

--- resources/SearchSettings.vue
+++ resources/SearchSettings.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="sgm-mx-auto sgm-max-w-5xl">
+  <form class="sgm-max-w-5xl">
     <div class="sgm-space-y-12">
       <div
         class="sgm-grid sgm-grid-cols-1 sgm-gap-x-8 sgm-gap-y-10 sgm-border-b sgm-border-gray-900/10 sgm-pb-12 md:sgm-grid-cols-3"
@@ -20,19 +20,14 @@
           class="sgm-grid sgm-max-w-2xl sgm-grid-cols-1 sgm-gap-x-6 sgm-gap-y-8 sm:sgm-grid-cols-6 md:sgm-col-span-2"
         >
           <div class="sm:sgm-col-span-4">
-            <label
-              for="website"
-              class="sgm-block sgm-text-sm sgm-font-medium sgm-leading-6 sgm-text-gray-900"
-              >Website</label
-            >
             <div class="sgm-mt-2">
               <div class="sgm-flex sgm-flex-col sgm-gap-2">
-                <label for="username">Username</label>
-                <InputText
-                  id="username"
-                  v-model="value"
-                  aria-describedby="username-help"
-                />
+                <label
+                  class="sgm-block sgm-text-sm sgm-font-medium sgm-leading-6 sgm-text-gray-900"
+                  for="username"
+                  >Username</label
+                >
+                <InputText id="username" aria-describedby="username-help" />
                 <small id="username-help"
                   >Enter your username to reset your password.</small
                 >
@@ -80,13 +75,7 @@
               >First name</label
             >
             <div class="sgm-mt-2">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autocomplete="given-name"
-                class="sgm-block sgm-w-full sgm-rounded-md sgm-border-0 sgm-py-1.5 sgm-text-gray-900 sgm-shadow-sm sgm-ring-1 sgm-ring-inset sgm-ring-gray-300 sgm-placeholder:text-gray-400 sgm-focus:ring-2 sgm-focus:ring-inset sgm-focus:ring-indigo-600 sm:sgm-text-sm sm:sgm-leading-6"
-              />
+              <InputText id="username" aria-describedby="username-help" />
             </div>
           </div>
 
@@ -97,13 +86,7 @@
               >Last name</label
             >
             <div class="sgm-mt-2">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autocomplete="family-name"
-                class="sgm-block sgm-w-full sgm-rounded-md sgm-border-0 sgm-py-1.5 sgm-text-gray-900 sgm-shadow-sm sgm-ring-1 sgm-ring-inset sgm-ring-gray-300 sgm-placeholder:text-gray-400 sgm-focus:ring-2 sgm-focus:ring-inset sgm-focus:ring-indigo-600 sm:sgm-text-sm sm:sgm-leading-6"
-              />
+              <InputText id="username" aria-describedby="username-help" />
             </div>
           </div>
         </div>
